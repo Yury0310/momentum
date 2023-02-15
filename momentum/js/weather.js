892698317cd267e973df412c52b9fe1ff18d8f95
Yury0310 <ostap4uk41@gmail.com>
2023-02-15 18:00:49 +0300

    fix weather

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -4,37 +4,32 @@ const weatherDescription = document.querySelector(".weather-description");
 const wind = document.querySelector(".wind");
 const humidity = document.querySelector(".humidity");
 
-const city = document.querySelector(".city");
+let city = document.querySelector(".city");
 
 async function getWeather() {
   // window.addEventListener("beforeunload", setLocalStorage);
   // window.addEventListener("load", getLocalStorage);
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=2bd0c9f9541bb28fb71701b1a2e9bab3&units=metric`;
-  const res = await fetch(url);
-  const data = await res.json();
-  weatherIcon.className = "weather-icon owf";
-  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
-  temperature.textContent = `${Math.round(data.main.temp)}°C`;
-  weatherDescription.textContent = data.weather[0].description;
-  wind.textContent = `Wind speed: ${Math.round(data.wind.speed)} m/s`;
-  humidity.textContent = `Humidity: ${data.main.humidity}%`;
+  setTimeout(async () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=2bd0c9f9541bb28fb71701b1a2e9bab3&units=metric`;
+    console.log(city.value);
+    const res = await fetch(url);
+    const data = await res.json();
+    weatherIcon.className = "weather-icon owf";
+    weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    temperature.textContent = `${Math.round(data.main.temp)}°C`;
+    weatherDescription.textContent = data.weather[0].description;
+    wind.textContent = `Wind speed: ${Math.round(data.wind.speed)} m/s`;
+    humidity.textContent = `Humidity: ${data.main.humidity}%`;
+  }, 1000);
 }
 
 function setLocalStorage() {
   localStorage.setItem("city", city.value);
-  localStorage.setItem("temp", temperature.textContent);
-  localStorage.setItem("desc", weatherDescription.textContent);
-  localStorage.setItem("wind", wind.textContent);
-  localStorage.setItem("hum", humidity.textContent);
 }
 
 function getLocalStorage() {
   if (localStorage.getItem("city")) {
     city.value = localStorage.getItem("city");
-    temperature.textContent = localStorage.getItem("temp");
-    weatherDescription.textContent = localStorage.getItem("desc");
-    wind.textContent = localStorage.getItem("wind");
-    humidity.textContent = localStorage.getItem("hum");
   }
 }
 
