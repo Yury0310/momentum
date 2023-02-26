d1579da5d67955cb6c9050ed4f97fb866dc53eab
Yury0310 <ostap4uk41@gmail.com>
2023-02-26 15:41:16 +0300

    fix todo

--- momentum/js/todolist.js
+++ momentum/js/todolist.js
@@ -1,27 +1,48 @@
 const input = document.querySelector("#input");
 const btn = document.querySelector(".btn");
 const btn2 = document.querySelector(".btn2");
-
 const result = document.querySelector("#result");
 
-btn.addEventListener("click", () => {
-  if (input.value === "") return;
-  liElement(input.value);
-  input.value = "";
-});
+let values = JSON.parse(localStorage.getItem("values")) || [];
 
-input.addEventListener("keydown", todo);
-
-function todo(e) {
-  if (e.keyCode === 13) {
-    if (input.value === "") return;
-    liElement(input.value);
-    input.value = "";
+if (values.length !== 0) {
+  for (let i = 0; i < values.length; i++) {
+    liElement(values[i]);
   }
 }
 
-function liElement(value) {
-  console.log(value);
+function addingToStorage() {
+  let valueObj = {
+    id: values.length ? values.length : 0,
+    value: input.value,
+  };
+  values.push(valueObj);
+  localStorage.setItem("values", JSON.stringify(values));
+  liElement(valueObj);
+}
+
+btn.addEventListener("click", () => {
+  if (input.value === "") return;
+  addingToStorage();
+  input.value = "";
+});
+
+input.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13) {
+    if (input.value === "") return;
+    addingToStorage();
+    input.value = "";
+  }
+});
+
+btn2.addEventListener("click", () => {
+  result.innerHTML = "";
+  localStorage.setItem("values", JSON.stringify([]));
+});
+
+function liElement(valueObj) {
+  const { id, value } = valueObj;
+
   const li = document.createElement("li");
   result.appendChild(li);
   li.className = "li";
@@ -39,11 +60,9 @@ function liElement(value) {
   });
 
   btn.addEventListener("click", () => {
+    values = values.filter((item) => item.id !== id);
+    localStorage.setItem("values", JSON.stringify(values));
+
     result.removeChild(li);
-  });
-  btn2.addEventListener("click", () => {
-    for (let i = 0; i < li1.length; i++) {
-      result.removeChild(li);
-    }
   });
 }
